19da9e5d9d0cf9ecce03b8ec9f84934b4b2516ad
alefcardosorodrigues <alefstarkin@gmail.com>
2026-04-05 18:55:04 -0300

    fix: build errors regarding typescript extensions and vite types

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App.tsx'
+import App from './App'
 import { MonthProvider } from './contexts/MonthContext'
 import './index.css'
 
